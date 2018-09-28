d0bee22a38170001cbaec04bbc64d30c5ba23fd2
Chris Wells Wood <cwwoodesq@gmail.com>
2018-09-28 11:35:13 +0100

    Adds mode explaination.

--- web/elm-src/Tutorial.elm
+++ web/elm-src/Tutorial.elm
@@ -10,7 +10,9 @@ module Tutorial exposing
 import Css
 import Fancy
 import Html.Styled as Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
 import Html.Styled.Events exposing (..)
+import Markdown
 import Model
 
 
@@ -37,13 +39,6 @@ type alias Config msg =
     , next : msg
     , cancel : msg
     }
-
-
-tutorial : Config msg -> Tutorial msg
-tutorial config =
-    Tutorial [] (welcome config) <|
-        List.map (\s -> s config)
-            []
 
 
 moveForward : Tutorial msg -> Tutorial msg
@@ -76,22 +71,86 @@ moveBackwards (Tutorial previousSections currentSection nextSections) =
             Tutorial previousSections currentSection nextSections
 
 
+tutorial : Config msg -> Tutorial msg
+tutorial config =
+    Tutorial [] (welcome config) <|
+        List.map (\s -> s config)
+            [ modes ]
+
+
+tutorialWindow : List (Styled.Attribute msg) -> List (Styled.Html msg) -> Styled.Html msg
+tutorialWindow =
+    styled Styled.div
+        [ Css.backgroundColor Fancy.colourPalette.c2
+        , Css.boxShadow5
+            Css.zero
+            Css.zero
+            (Css.px 10)
+            Css.zero
+            (Css.rgba 133 133 133 1)
+        , Css.padding (Css.px 5)
+        ]
+
+
 welcome : Config msg -> Section msg
-welcome { previous, next, cancel } =
+welcome { next, cancel } =
     { tutorialWindow =
-        tutorialWindow []
-            [ p []
+        tutorialWindow [ css [ Css.width (Css.pct 80) ] ]
+            [ Fancy.h2 [] [ text "Welcome!" ]
+            , Fancy.p []
                 [ "Hello and welcome to BUDE Alanine Scan. This tutorial will "
                     ++ "guide you through the basics of the application. "
                     |> text
                 ]
+            , Fancy.button [ disabled True ] [ text "Previous" ]
+            , Fancy.button [ onClick next ] [ text "Next" ]
             , Fancy.button [ onClick cancel ] [ text "Cancel" ]
             ]
     , model = Model.emptyModel
     }
 
 
-tutorialWindow : List (Styled.Attribute msg) -> List (Styled.Html msg) -> Styled.Html msg
-tutorialWindow =
-    styled Styled.div
-        [ Css.backgroundColor Fancy.colourPalette.c2 ]
+modes : Config msg -> Section msg
+modes { previous, next, cancel } =
+    { tutorialWindow =
+        tutorialWindow
+            [ css
+                [ Css.width (Css.pct 80)
+                , Css.maxHeight (Css.pct 80)
+                ]
+            ]
+            [ Fancy.h2 [] [ text "Modes" ]
+            , modeExplaination
+            , Fancy.button [ onClick previous ] [ text "Previous" ]
+            , Fancy.button [ onClick next ] [ text "Next" ]
+            , Fancy.button [ onClick cancel ] [ text "Cancel" ]
+            ]
+    , model = Model.emptyModel
+    }
+
+
+modeExplaination : Html msg
+modeExplaination =
+    """BUDE Alanine Scan (BALS) has two modes of operation:
+
+* **Scan** - performs standard computational alanine scan, where every residue
+of the protein of interest (called the _ligand_ in the app) is mutated to
+alanine in turn.  The interaction energy between each mutant of the _ligand_ and
+another protein (called the _receptor_ in the app) is calculated using the [BUDE
+all-atom force
+field](http://journals.sagepub.com/doi/abs/10.1177/1094342014528252). The
+difference in the energy of with wild-type sequence _ligand_/_receptor_
+interaction and the mutant _ligand_/_receptor_ interaction, known as the ΔΔG, is
+calculated, which quantifies the energetic contribution of the residue to
+_ligand_/_receptor_ interaction.
+
+* **Constellation** - Once a Scan job is complete you can run a Constellation
+job.  In Constellation mode you can combine multiple alanine mutations and look
+for clusters of _ligand_ residues that cooperatively interact with the
+_receptor_ _i.e._ the sum of their individual ΔΔG values is less than the
+interaction energy with the _receptor_ when all residues are mutated to alanine
+simultaneously. This is useful for highlighting regions of the _ligand_ that are
+key to forming the interaction with the _receptor_.
+"""
+        |> Markdown.toHtml []
+        |> Styled.fromUnstyled
