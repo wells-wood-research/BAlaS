d5d2da4ed216fea4acb72ae6e3b1d3acbdeb4a10
Chris Wells Wood <cwwoodesq@gmail.com>
2019-11-07 08:12:33 +0000

    Adds flag to docker compose for config and changes default mode.

--- web/config.py
+++ web/config.py
@@ -4,13 +4,13 @@ import os
 
 
 def get_config():
-    """Used to create the config object for the Flask app.
+    """Create the config object for the Flask app.
 
     Notes
     -----
-    This function looks for the `BALS_CONFIG` environmental
+    This function looks for the `BALAS_CONFIG` environmental
     variable and sets the config based its value. For example,
-    if the `BALS_CONFIG = production`, then `ProductionConfig`
+    if the `BALAS_CONFIG = production`, then `ProductionConfig`
     will be used. If the environmental variable is not set
     then `DevelopmentConfig` will be used. This variable is
     usually set in `docker-compose.yml`.
@@ -19,26 +19,30 @@ def get_config():
     -------
     config
         Object containing BALS config options.
+
     """
     config_options = {
         "development": DevelopmentConfig(),
         "production": ProductionConfig(),
-        "default": DevelopmentConfig()
+        "default": ProductionConfig(),
     }
-    config_name = os.getenv(key='BALS_CONFIG', default='default')
+    config_name = os.getenv(key="BALAS_CONFIG", default="default")
     return config_options[config_name]
 
 
 class BaseConfig:
     """Contains shared configuration options."""
+
     pass
 
 
 class DevelopmentConfig(BaseConfig):
     """Contains development configuration options."""
+
     DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
     """Contains production configuration options."""
+
     DEBUG = False
